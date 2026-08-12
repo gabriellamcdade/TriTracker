d17d2e73f15eefceea8b77870758fa7aeff40ccd
gabriellamcdade <c5008555@newcastle.ac.uk>
2026-08-12 19:56:27 +0100

    add weekly training volume analysis

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import csv
 
+from collections import defaultdict
+from datetime import datetime
+
 run_distance = run_duration = run_hr_total = run_count = 0
 bike_distance = bike_duration = bike_hr_total = bike_count = 0
 swim_distance = swim_duration = swim_hr_total = swim_count = 0
@@ -55,3 +58,55 @@ print("\nSWIMMING")
 print(f"Distance: {swim_distance:.1f} km")
 print(f"Average pace: {swim_pace_per_100m:.2f} min/100m")
 print(f"Average heart rate: {swim_avg_hr:.0f} bpm")
+
+
+weekly_data = defaultdict(lambda: defaultdict(lambda: {
+    "distance": 0,
+    "duration": 0
+}))
+
+with open("data/activities.csv", "r", newline="") as file:
+    activities = csv.DictReader(file)
+
+    for activity in activities:
+        date = datetime.strptime(activity["date"], "%Y-%m-%d")
+        week_number = date.isocalendar().week
+        year = date.isocalendar().year
+        week = f"{year} - Week {week_number}"
+
+        sport = activity["sport"]
+        distance = float(activity["distance_km"])
+        duration = int(activity["duration_min"])
+
+        weekly_data[week][sport]["distance"] += distance
+        weekly_data[week][sport]["duration"] += duration
+
+print("\nWEEKLY TRAINING SUMMARY")
+
+sport_titles = {
+    "Run": "RUNNING",
+    "Bike": "CYCLING",
+    "Swim": "SWIMMING",
+}
+
+for week, sports in weekly_data.items():
+    print(f"\n{week}")
+
+    total_training_time = 0
+
+    for sport in ["Run", "Bike", "Swim"]:
+        if sport in sports:
+            distance = sports[sport]["distance"]
+            duration = sports[sport]["duration"]
+            total_training_time += duration
+
+            hours = duration // 60
+            minutes = duration % 60
+
+            print(f"\n{sport_titles[sport]}")
+            print(f"Distance: {distance:.1f} km")
+            print(f"Time: {hours}h {minutes}m")
+
+    total_hours = total_training_time // 60
+    total_minutes = total_training_time % 60
+    print(f"\nTotal training time: {total_hours}h {total_minutes}m")
