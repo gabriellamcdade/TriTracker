8c63c7b00126d7fdf07061ef6db9f24da5a8dc2b
gabriellamcdade <c5008555@newcastle.ac.uk>
2026-08-12 01:32:09 +0100

    add running pace calculation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import csv
 from collections import defaultdict
-
+#total  of each sport
 totals = defaultdict(float)
 
 with open("data/activities.csv", "r", newline="") as file:
@@ -20,3 +20,18 @@ names = {
 
 for sport, distance in totals.items():
     print(f"{names[sport]}: {distance:.1f} km")
+#running pace calc
+run_distance = 0
+run_duration = 0
+
+with open("data/activities.csv", "r", newline="") as file:
+    activities = csv.DictReader(file)
+
+    for activity in activities:
+        if activity["sport"] == "Run":
+            run_distance += float(activity["distance_km"])
+            run_duration += float(activity["duration_min"])
+
+average_pace = run_duration / run_distance
+
+print(f"Average running pace: {average_pace:.2f} min/km")
