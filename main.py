ee5d25810db62e77c4e6e88bfbf5030cf7616674
gabriellamcdade <c5008555@newcastle.ac.uk>
2026-08-12 01:34:11 +0100

    add cycling  pace calculation

--- main.py
+++ main.py
@@ -1,37 +1,34 @@
 import csv
-from collections import defaultdict
-#total  of each sport
-totals = defaultdict(float)
 
-with open("data/activities.csv", "r", newline="") as file:
-    activities = csv.DictReader(file)
-
-    for activity in activities:
-        totals[activity["sport"]] += float(activity["distance_km"])
-
-print("TriTracker Training Summary\n")
-
-
-names = {
-    "Run": "Running",
-    "Bike": "Cycling",
-    "Swim": "Swimming",
-}
-
-for sport, distance in totals.items():
-    print(f"{names[sport]}: {distance:.1f} km")
-#running pace calc
 run_distance = 0
 run_duration = 0
+bike_distance = 0
+bike_duration = 0
 
 with open("data/activities.csv", "r", newline="") as file:
     activities = csv.DictReader(file)
 
     for activity in activities:
-        if activity["sport"] == "Run":
-            run_distance += float(activity["distance_km"])
-            run_duration += float(activity["duration_min"])
+        sport = activity["sport"]
+        distance = float(activity["distance_km"])
+        duration = float(activity["duration_min"])
 
-average_pace = run_duration / run_distance
+        if sport == "Run":
+            run_distance += distance
+            run_duration += duration
 
-print(f"Average running pace: {average_pace:.2f} min/km")
+        elif sport == "Bike":
+            bike_distance += distance
+            bike_duration += duration
+
+run_pace = run_duration / run_distance
+bike_speed = bike_distance / (bike_duration / 60)
+
+print("TRITRACKER SUMMARY")
+print("\nRUNNING")
+print(f"Distance: {run_distance:.1f} km")
+print(f"Average pace: {run_pace:.2f} min/km")
+
+print("\nCYCLING")
+print(f"Distance: {bike_distance:.1f} km")
+print(f"Average speed: {bike_speed:.1f} km/h")
